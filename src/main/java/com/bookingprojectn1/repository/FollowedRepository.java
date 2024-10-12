1b3094e3fee44fdcbf5337f0c72649923537ceec
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2024-10-12 15:15:35 +0500

    startup boshlandi

--- src/main/java/com/bookingprojectn1/repository/FollowedRepository.java
+++ src/main/java/com/bookingprojectn1/repository/FollowedRepository.java
@@ -12,7 +12,7 @@ public interface FollowedRepository extends JpaRepository<Followed, Integer> {
         @Query(value = "select * from library_followed_list where library_id =?1",nativeQuery = true)
         List<Followed> findAll(Long libraryId);
 
-        @Query(value = "select * from followed left join library_followed_list lfl where" +
-                " user_id =?1 and lfl.library_id =?2",nativeQuery = true)
+        @Query(value = "select * from followed f left join library_followed_list  lfl on  lfl.library_id =?2 where" +
+                " f.user_id =?1 and lfl.library_id =?2",nativeQuery = true)
         List<Followed> findAllFollowed(Long userId, Long libraryId);
 }
